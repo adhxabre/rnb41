b757f20dd7dddf8908fc527babfb9e9f8f5c062e
adhxabre <abeldustin06@gmail.com>
2022-12-26 18:54:21 +0700

    Styling

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 
-import State from "./src/screen/state";
+import State from "./src/screen/form";
 
 export default function App() {
   return (
-    <View style={{ marginTop: "auto", marginBottom: "auto" }}>
+    <View>
       <StatusBar />
       <State />
     </View>
